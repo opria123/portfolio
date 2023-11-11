303c1cebaa6b09871f89224580d793f741eb11a0
Joshua Opria <opriaj@example.com>
2023-11-11 11:31:53 -0500

    Chaning the app to dynamically retrieve the data

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 // Core viewer
-import { Viewer } from '@react-pdf-viewer/core';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
 
 // Plugins
 
 // Import styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { pdfjs } from 'react-pdf';
 
 // Create new plugin instance
 
 export default function Resume() {
-    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 
     return (
         <>
-            <Viewer
-                fileUrl='https://raw.githubusercontent.com/opria123/portfolio/main/public/Josh-opria-resume.pdf'
-            />
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                <Viewer fileUrl="https://raw.githubusercontent.com/opria123/portfolio/main/public/Josh-opria-resume.pdf" />
+            </Worker>
+
         </>
     );
 };
